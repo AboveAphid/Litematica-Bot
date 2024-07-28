90501d4d8ab795832fd28bc2d04621b3fd8fe243
A_Aphid <112936848+AboveAphid@users.noreply.github.com>
2024-07-28 12:30:21 +1000

    Update litematic-converter.py
    Updated converter to be more clear/well made 😅.
    
    Fixed:
    "Folder already exists! Would you like to overide with new processed data? (y/N) " doing the opposite of what you choose... Yea that was silly of me.
    
    Removed unnecessary pickle file creation
    
    Fixed deprecated Region code for reading litematica

--- litematic-converter.py
+++ litematic-converter.py
@@ -1,37 +1,29 @@
-from litemapy import Schematic
-import pandas as pd
-import json, pickle, os
-
-
+from litemapy import Schematic, Region
+import json, os
 
 FOLDER = "LitematicaSchematics\\"
-FILENAME = input('Filename: ') #"test"
-
 SAVEFOLDER = 'Generated Files'
-
+FILENAME = input('Filename: ') #"test"
 DEFAULTFILENAME = FILENAME
 
 # Load the schematic and get its first region
 schem = Schematic.load(FOLDER + FILENAME + ".litematic")
+reg: Region = list(schem.regions.values())[0]
 
-reg = list(schem.regions.values())[0]
-
-
-# Print out the basic shape
+# Print out the basic shape and read litematica
 sections = {}
-
 section = []
 i = 0
-for y in reg.yrange():
-    for x in reg.xrange():
-        for z in reg.zrange():
-            b = reg.getblock(x, y, z)
-            if b.blockid == "minecraft:air":
+for y in reg.range_y():
+    for x in reg.range_x():
+        for z in reg.range_z():
+            b = reg[x, y, z]
+            if b.id == "minecraft:air":
                 print(" ", end="")
             else:
                 print("#", end='')
 
-                section.append([(x, y, z), b.blockid])
+                section.append([(x, y, z), b.id])
         print()
     
     print('\n\n')
@@ -40,34 +32,28 @@ for y in reg.yrange():
     section = []
 
 
-
-
-
-
-i = 0
-while 1:
-    if not os.path.exists(f'{SAVEFOLDER}\\{FILENAME}'):
-        os.mkdir(f'{SAVEFOLDER}\\{FILENAME}')
-        break
+if os.path.exists(f'{SAVEFOLDER}\\{FILENAME}'):
+    if input('Folder already exists! Would you like to overide with new processed data? (y/N) ').lower() == 'y':
+        if input('Are you SURE you want to overide these files? (y/N) ').lower() == 'y':
+            print('Overiding...')
+        else:
+            print("Operation canceled!")
     else:
-        if input('Folder already exists! Would you like to overide with new processed data? (Y/n)') == 'y':
+        # Find a folder that isn't used/existing yet
+        FILENAME = f'{DEFAULTFILENAME}_{i}'
+        i = 0
+        while os.path.exists(os.path.join(SAVEFOLDER, FILENAME)):
             FILENAME = f'{DEFAULTFILENAME}_{i}'
             print('Trying new name:', FILENAME)
-        else:
-            print('Overiding...')
-            break
-    i += 1
-        
+            i += 1
 
-# My very janky way of saving it as a json
-with open(f'{SAVEFOLDER}\\{FILENAME}\\{FILENAME}.pickle', 'wb') as f:
-    pickle.dump(sections, f, protocol=pickle.HIGHEST_PROTOCOL) ##@##R$%^&*(*REW#$%^&*&^%$#$%^&**&) sections or json_sections???? From testing it would seem to be sections lol
+# Create folder
+print("Saved to:", FILENAME)
+os.mkdir(f'{SAVEFOLDER}\\{FILENAME}')
 
-with open(f'{SAVEFOLDER}\\{FILENAME}\\{FILENAME}.pickle', 'rb') as f:
-    data = pd.read_pickle(f)
-
+# Export out into json for other files to read
 with open(f'{SAVEFOLDER}\\{FILENAME}\\{FILENAME}.json', 'w') as f:
-    json.dump(data, f)
+    json.dump(sections, f)
 
 with open(f'{SAVEFOLDER}\\{FILENAME}\\{FILENAME}-readable.json', 'w') as f:
-    json.dump(data, f, indent=4)
+    json.dump(sections, f, indent=4)
